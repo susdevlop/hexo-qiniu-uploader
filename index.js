df0fc44e2071a39355c20e2caf2597f75d3d40e5
TzuHsing Su <ai66900144@gmail.com>
2021-05-06 10:01:14 +0800

    测试

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+global.hexo = hexo;
+// var log = hexo.log;
+console.log('-------开始-------')
+if (!hexo.config.cdn || !hexo.config.cdn.enable) {
+    return;
+}
+
+if (hexo.config.cdn.img_url === undefined) {
+    hexo.config.cdn.img_url = hexo.config.cdn.url;
+} else if (
+    hexo.config.cdn.img_url.indexOf("http") !== 0 &&
+    hexo.config.cdn.img_url.indexOf("//") !== 0
+) {
+    hexo.config.cdn.img_url = hexo.config.cdn.url + hexo.config.cdn.img_url ;
+}
+
+if (hexo.config.cdn.onlypost) {
+    hexo.extend.filter.register('after_post_render', require('./lib/process').processPost);
+}
+else {
+    hexo.extend.filter.register('after_render:html', require('./lib/process').processSite);
+    hexo.extend.filter.register('after_render:css', require('./lib/process').processSheet);
+}
+// hexo.extend.filter.register('after_render', function(str, data){
+//     console.log('开始上传静态文件到七牛云');
+//     require('./lib/uploader').upload()
+// });
