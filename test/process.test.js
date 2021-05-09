b0c2e81c51730045f4df295346d0a0089adce8cd
TzuHsing Su <ai66900144@gmail.com>
2021-05-09 10:03:39 +0800

    修复img标签换行无法匹配的问题

--- test/process.test.js
+++ test/process.test.js
@@ -1,6 +1,6 @@
 const { processSite } = require('../lib/process');
 let context = null;
-const cdnRoot = 'https://cdn.jsdelivr.net/gh/zhepama/igiven.github.io@gh-pages/';
+const cdnRoot = 'https://www.baidu.com';
 beforeEach(() => {
   context = {
     config: {
@@ -16,4 +16,13 @@ test('should use default image if no specify custom image', () => {
   const actual = processSite.bind(context)(htmlContent);
   expect(actual).toBe(htmlContent);
 });
+
+test('should use default image if type a return key',()=>{
+  const htmlContent = `            <p style="font-size: 30px; color:#fff;margin-top: 30px;"><img
+                        src="/images/main/ic_address.png"
+                        style="width: 50px;height: 50px;padding-right: 12px;"/>深圳市前海深港合作区前湾一路一号A栋
+            </p>`;
+  const actual = processSite.bind(context)(htmlContent);
+  expect(actual).toMatch('https://www.baidu.com/images/main/ic_address.png')
+})
 
